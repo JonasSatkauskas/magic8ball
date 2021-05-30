2276d7292dcb60502424420d41be709ebbd7c666
Joe <joncius224@gmail.com>
2021-05-30 20:07:40 +0300

    Initial commit

---
+++ magic8ball.py
@@ -0,0 +1,47 @@
+import random
+import time
+
+
+# Galimi programos atsakymai
+pasirinkimu = ['Taip', 'Ne', 'Aš dabar užsiėmęs... Paklausk manęs vėliau...', 'Tikrai taip!', 'Tikrai ne!', 'Suformuluok klausimą kitaip']
+time.sleep(0.5)
+# Pasisveikinimas su paaiškinimu
+print('Labas, aš be rūpesčių atsakysiu į klausimą su pradžia "ar"')
+print("\nJeigu nori išeiti, įvesk raidę 'q'")
+
+index = 1
+
+while True:
+	# Priminimas apie išėjimą
+	if index % 4 == 0:
+		time.sleep(0.5)
+		print("Jeigu nori išeiti, įvesk raidę 'q'")
+	# Atsakymo priskyrimas kimtamajam, kad jį paskui būtų galima išprintint
+	pasirinkimas = random.choice(pasirinkimu)
+	print()
+	
+	time.sleep(0.7)
+	# Vartotojo suformuluotas klausimas
+	klausimas = input('Koks Tavo klausimas?\n')
+	# Išėjimas iš loop'o
+	if klausimas.upper() == 'Q':
+		time.sleep(0.5)
+		print('\nIki kitų kartų! Tikiuosi atsakiau i Tavo klausim-ą/us')
+		break
+	# Jeigu klausimas prasideda žodžiu 'ar', programa atsako į klausimą.
+	if 'AR' in klausimas.upper():
+		time.sleep(0.5)
+		print('\n\t'+pasirinkimas)
+		print()
+	# Jei klausime yra intarpas 'robot', programa pajuokauja
+	elif 'robot' in klausimas:
+		print("Ehm... Error")
+		print()
+	# Jeig klausimas neprasideda žodžiu 'ar', programa pasako kaip derėtų suformuluoti klausimą 
+	else:
+		time.sleep(0.5)
+		print('\nTurite užduoti klausimą su pradžia AR (gali būti ir iš mažųjų)!')
+		print()
+	# Prie index pridedama 1 (kas 4 klausimą programa primena apie galimybę išeit įvedus 'q')
+	index += 1
+		
